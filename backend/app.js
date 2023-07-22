73cebfddedf9db07e1937472cd1c20f8badce443
Jules Bondarenko <psneg76@bk.ru>
2023-07-23 00:36:27 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,12 @@ const routes = require('./routes/index');
 
 const app = express();
 
-const { PORT = 3000 } = process.env; // urlMongo = 'mongodb://0.0.0.0:27017'
+const { PORT = 3000, urlMongo = 'mongodb://0.0.0.0:27017' } = process.env; // urlMongo = 'mongodb://0.0.0.0:27017'
 
 app.use(cors({
   origin: [
     'http://localhost:3000',
+    'https://localhost:3000',
     'http://back.jules-bo.nomoredomains.xyz',
     'https://back.jules-bo.nomoredomains.xyz',
     'http://jules-bo.nomoredomains.xyz',
@@ -25,13 +26,13 @@ app.use(cors({
   credentials: true,
 }));
 
-// mongoose.connect(`${urlMongo}/mestodb`, {
-//   useNewUrlParser: true,
-// });
-
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useUnifiedTopology: true,
+mongoose.connect(`${urlMongo}/mestodb`, {
+  useNewUrlParser: true,
 });
+
+// mongoose.connect('mongodb://localhost:27017/mestodb', {
+//   useUnifiedTopology: true,
+// });
 
 app.use(requestLogger);
 app.use(cookieParser());
